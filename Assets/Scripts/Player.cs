a16256999f90ee420b653298e5261115c4b981b2
DxDzere <mnicoparodi@gmail.com>
2020-12-04 03:29:01 -0300

    Nuevo Repositorio
    Debido a pequeños problemas el se tuvo que crear un nuevo repositorio para poder solucionarlo

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,101 @@
+﻿
+using UnityEngine;
+using Utils.Checker; 
+
+public class Player : MonoBehaviour
+{
+//comentario arbitrario
+    public Rigidbody sphere;
+    private GroundDetection groundDetection; 
+
+    [Header("Componentes")]
+    public float gravity = 10.0f;
+    public float speed, currentSpeed;
+    public float rotate, currentRotate;
+    public Transform kartNormal;
+    [SerializeField] private Transform _groundDetector; 
+    public GameObject kartModel; 
+    public LayerMask layer; 
+
+    [Header("Parameters")]
+    public float acceleration = 30f;
+    public float steering = 80f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundDetection = new GroundDetection(_groundDetector, layer, 1.0f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MENUPAUSA.PAUSADO == false)
+        {
+            if (groundDetection.CheckGround())
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    speed = acceleration;
+                }
+
+                if (Input.GetKey(KeyCode.S))
+                {
+                    speed = -acceleration / 4;
+                }
+            }
+           
+            //Debug.Log(groundDetection.CheckGround());
+
+            transform.position = sphere.transform.position - new Vector3(0, 0.4f, 0);
+
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+                float amount = Mathf.Abs((Input.GetAxis("Horizontal")));
+                Steer(dir, amount);
+            }
+
+            currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f); speed = 0f;
+            currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f); rotate = 0f;
+        }
+        else
+        {
+            sphere.transform.position = transform.position;
+        }
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (MENUPAUSA.PAUSADO == false)
+        {
+            sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+            //gravity
+            sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+
+            //steering
+            transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
+
+            RaycastHit hitOn;
+            RaycastHit hitNear;
+
+            Physics.Raycast(transform.position + (transform.up * .1f), Vector3.down, out hitOn, 1.1f, layer);
+            Physics.Raycast(transform.position + (transform.up * .1f), Vector3.down, out hitNear, 2.0f, layer);
+
+            kartNormal.up = Vector3.Lerp(kartNormal.up, hitNear.normal, Time.deltaTime * 8.0f);
+            kartNormal.Rotate(0, transform.eulerAngles.y, 0);
+        }
+    }
+
+    void Steer(int dir, float amaount)
+    {
+        rotate = (steering * dir) * amaount; 
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_groundDetector.position, 1.0f);
+    }
+}
---
+++ Assets/Scripts/PowerUp/ManagerPwPlayer.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ManagerPwPlayer : MonoBehaviour
+{
+    public GameObject[] PowerUps;
+    public GameObject imageItem,      Imagen_Item;
+    public GameObject escudo;
+    bool escudoAct = false;
+    int x = 4;
+
+    float timer = 3.0f;
+    void Start()
+    {
+        escudo.SetActive(false);
+    }
+    void Update()
+    {
+        float Posx = gameObject.transform.position.x;
+        float Posy = gameObject.transform.position.y;
+        float Posz = gameObject.transform.position.z;
+        Quaternion rotation = new Quaternion();
+        Vector3 positionObstaculo = new Vector3(Posx, Posy, Posz);
+        switch (x)
+        {
+            case 0:
+                imageItem.GetComponent<TextMeshProUGUI>().text = "Charco";
+                Imagen_Item.GetComponent<Image>().sprite = Resources.Load<Sprite>("4");
+                if (Input.GetKey("x"))
+                {
+                    Instantiate(PowerUps[0], positionObstaculo, rotation);
+                    x = 4;
+                }
+                break;
+            case 1:
+                imageItem.GetComponent<TextMeshProUGUI>().text = "Proyectil";
+                Imagen_Item.GetComponent<Image>().sprite = Resources.Load<Sprite>("3");
+                if (Input.GetKey("x"))
+                {
+                    Instantiate(PowerUps[1], positionObstaculo, rotation);
+                    x = 4;
+                }
+                break;
+            case 2:
+                imageItem.GetComponent<TextMeshProUGUI>().text = "Escudo";
+                Imagen_Item.GetComponent<Image>().sprite = Resources.Load<Sprite>("2");
+                if (Input.GetKey("x"))
+                {
+                    Instantiate(PowerUps[2], positionObstaculo, rotation);
+                    escudo.SetActive(true);
+                    escudoAct = true;
+                    Debug.Log(escudoAct);
+                    x = 4;
+                }
+                break;
+            case 3:
+                imageItem.GetComponent<TextMeshProUGUI>().text = "Tele-Proyectil";
+                Imagen_Item.GetComponent<Image>().sprite = Resources.Load<Sprite>("3");
+
+                //
+                //
+                break;
+            case 4:
+                imageItem.GetComponent<TextMeshProUGUI>().text = "No item";
+                Imagen_Item.GetComponent<Image>().sprite = Resources.Load<Sprite>("0");
+                break;
+        }
+
+        if(escudoAct == true && x == 4)
+        {
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                Debug.Log("Etapa3");
+                escudo.SetActive(false);
+                timer = 3;
+            }
+        }
+    }
+
+
+    void OnTriggerEnter(Collider Colider)
+    {
+        if (Colider.gameObject.tag == "PoweUp")
+        {
+            Debug.Log("hola");
+            x = Random.Range(0, 4);
+            Debug.Log(x);
+        }
+
+    }
+}
+
+
